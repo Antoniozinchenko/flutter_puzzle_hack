713fc99fe78c2da3fc5c25fc4d2ecd2293910218
Anton Zinchenko <12981061+Antoniozinchenko@users.noreply.github.com>
2022-01-13 22:40:54 +0200

    save score to shared prefs (#3)

--- lib/bloc/timer_state.dart
+++ lib/bloc/timer_state.dart
@@ -2,19 +2,35 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:puzzle_hack/bloc/winner_state.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TimerState extends ValueNotifier<int?> {
-  TimerState() : super(null);
+  TimerState() : super(null) {
+    SharedPreferences.getInstance().then((prefs) {
+      bestScore = prefs.getInt(bestScoreKey);
+      notifyListeners();
+    });
+  }
+
+  static const bestScoreKey = 'best';
 
   int? bestScore;
 
   StreamSubscription? _subscription;
 
-  void checkTimer(WinnerState winnerState) {
+  Future<void> _saveScore() async {
+      if (bestScore != null) {
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setInt(bestScoreKey, bestScore!);
+      }
+  }
+
+  Future<void> checkTimer(WinnerState winnerState) async {
     if (winnerState.value) {
       bestScore = bestScore == null || value! < bestScore! ? value : bestScore;
       _subscription?.cancel();
       notifyListeners();
+      _saveScore();
     } else if (winnerState.movesCount < 1) {
       _subscription?.cancel();
       value = null;
